8c339f6f002153eb3ca643dc289f3c244de60e77
Gaurav Suthar <107467642+gaurav78528@users.noreply.github.com>
2023-07-15 02:08:32 +0530

    updated files

--- index.js
+++ index.js
@@ -20,15 +20,18 @@ const app = express();
 
 // Middlewares
 // app.use(morgan("dev"));
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
+
 
 app.use(express.json());
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+const corsConfig = {
+  origin: '',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
+}
+app.use(cors(corsConfig))
+app.options("", cors(corsConfig))
+
+// app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(cookieParser());
 // Routes
 
